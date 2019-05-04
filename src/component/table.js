ca767e7cd5adc82211b65cc4eba9e3d6b360274d
EmilyYoung71415 <widestdream.me@gmail.com>
2019-05-04 09:44:04 +0800

    :sparkles: feat:表格布局初始化渲染

---
+++ src/component/table.js
@@ -0,0 +1,137 @@
+import { Draw } from '../draw/canvas';
+import help from '../utils/help';
+import alphabet from '../config/alphabet';
+const gLeftFixedCellWidth = 60;
+// const gCellPaddingWidth = 5;
+export default class Table{
+    constructor(el,row,col,style){
+        this.el = el;
+        this.context = el.getContext('2d');
+        this.draw = new Draw(el);// canvas操作的类
+        this.row = row;
+        this.col = col;
+        this.rowm = {}; // {rowIndex: {height: 200},....}
+        this.colm = {}; // {colIndex: {width: 200},....}
+        this.cellmm = {}; // {rowIndex: {colIndex: Cell}}
+        this.style = style;
+        this.styles = []; // style特殊单元格
+        this.borders = []; // border边框样式
+    }
+    render(){
+        this.clear()
+        this.renderContentGrid();//绘制content格子
+        this.renderFixedHeaders();//绘制第0行/第0列的 索引栏
+        this.renderContent();// 将表格数据填充进单元格
+    }
+    loadData(data){
+        if(data){
+            const {rowm, colm, cellmm, styles, borders} = data;
+            if (rowm) this.rowm = rowm;
+            if (colm) this.colm = colm;
+            if (cellmm) this.cellmm = cellmm;
+            if (styles) this.styles = styles;
+            if (borders) this.borders = borders;
+        }
+    }
+    clear(){
+        this.draw.clear()
+    }
+    renderContentGrid(){
+        const {draw, row, col} = this;
+        draw.save();
+        draw.attr({
+            lineWidth: 0.5,
+            strokeStyle: '#d0d0d0',
+        });
+        draw.translate(gLeftFixedCellWidth, row.height);
+        const colSumWidth = this.colTotalWidth();// 全部列总宽
+        const rowSumHeight = this.rowTotalHeight();// 全部行总高
+        // 在table里的画横、竖
+        // 每一行的横线起始两点坐标为(0,y),(colSumWidth, y)
+        this.rowEach(row.len, (i, y) => {
+            draw.line([0, y], [colSumWidth, y]);
+        });
+        this.colEach(col.len, (i, x) => {
+            draw.line([x, 0], [x, rowSumHeight]);
+        });
+        draw.restore();
+    }
+    renderFixedHeaders(){// 表格的索引栏 有浅灰色的背景颜色
+        const {draw, row, col} = this;
+        draw.save();
+        // 背景颜色   
+        draw.attr({ fillStyle: '#f4f5f8' })
+            // 填充背景
+            .fillRect(0, 0, gLeftFixedCellWidth, this.rowTotalHeight() + row.height)
+            .fillRect(0, 0, this.colTotalWidth() + gLeftFixedCellWidth, row.height);
+        draw.attr({
+            textAlign: 'center',
+            textBaseline: 'middle',
+            font: '500 12px sans-serif',
+            fillStyle: '#585757',
+            lineWidth: 0.5,
+            strokeStyle: '#d0d0d0',
+        });
+        // 第一列 生成行索引
+        this.rowEach(row.len, (i, y1, rowHeight) => {
+            const y = y1 + row.height;
+            const [tx, ty] = [0 + (gLeftFixedCellWidth / 2), y + (rowHeight / 2)];
+            if (i !== row.len) draw.fillText(i + 1, tx, ty);
+            // 分割线
+            draw.line([0, y], [gLeftFixedCellWidth, y]);
+        });
+        draw.line([gLeftFixedCellWidth, 0], [gLeftFixedCellWidth, this.rowTotalHeight() + row.height]);
+        // 第一行 生成列索引
+        this.colEach(col.len, (i, x1, colWidth) => {
+            const x = x1 + gLeftFixedCellWidth;
+            const [tx, ty] = [x + (colWidth / 2), 0 + (row.height / 2)];
+            if (i !== col.len) draw.fillText(alphabet.stringAt(i), tx, ty);
+            draw.line([x, 0], [x, row.height]);
+        });
+        draw.line([0, row.height], [this.colTotalWidth() + gLeftFixedCellWidth, row.height]);
+        // left-top-cell
+        draw.attr({ fillStyle: 'red' })
+            .fillRect(0, 0, leftFixedCellWidth, row.height);
+        draw.restore();
+    }
+    renderContent(){
+
+    }
+    colTotalWidth(){
+        // col是normal-size colm包含了特殊样式列的列样式
+        const { col, colm } = this;
+        const [cmTotal, cmSize] = help.sum(colm, v => v.width || 0);
+        return ((col.len - cmSize) * col.width) + cmTotal;
+    }
+    rowTotalHeight(){
+        const { row, rowm } = this;
+        const [rmTotal, rmSize] = help.sum(rowm, v => v.height || 0);
+        return ((row.len - rmSize) * row.height) + rmTotal;
+    }
+    // 列个数
+    colEach(colLen, cb) {
+        let x = 0;
+        for (let i = 0; i <= colLen; i += 1) {
+          const colWidth = this.getColWidth(i);//当前列的列宽
+          // 列索引 当前笔触的位置x 当前列的列宽
+          cb(i, x, colWidth);
+          x += colWidth;
+        }
+    }
+    rowEach(rowLen, cb) {
+        let y = 0;
+        for (let i = 0; i <= rowLen; i += 1) {
+          const rowHeight = this.getRowHeight(i);
+          cb(i, y, rowHeight);
+          y += rowHeight;
+        }
+    }
+    getColWidth(index) {//如果当前列未在特殊样式列 就是normal-size
+        const { col, colm } = this;
+        return colm[`${index}`] ? colm[`${index}`].width : col.width;
+    }
+    getRowHeight(index) {
+        const { row, rowm } = this;
+        return rowm[`${index}`] ? rowm[`${index}`].height : row.height;
+    }
+}
